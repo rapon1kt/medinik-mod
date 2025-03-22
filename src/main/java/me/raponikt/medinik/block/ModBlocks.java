a37bddb92ade65acad27b7b5a55b0a97009ddca6
rapon1kt <shinnatensei@gmail.com>
2025-03-22 01:09:22 -0300

    refactor: changing type of block of medriod ores, and add more strength

--- src/main/java/me/raponikt/medinik/block/ModBlocks.java
+++ src/main/java/me/raponikt/medinik/block/ModBlocks.java
@@ -2,10 +2,11 @@ package me.raponikt.medinik.block;
 
 import me.raponikt.medinik.Medinik;
 import me.raponikt.medinik.item.ModItems;
-import net.minecraft.client.resources.sounds.Sound;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -19,16 +20,20 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Medinik.MOD_ID);
 
     public static final RegistryObject<Block> MEDRIOD_BLOCK = registerBlock("medriod_block",
-            () -> new Block(BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops().sound(SoundType.NETHERITE_BLOCK)));
+            () -> new Block(BlockBehaviour.Properties.of().strength(5f)
+                    .requiresCorrectToolForDrops().sound(SoundType.NETHERITE_BLOCK)));
 
     public static final RegistryObject<Block> RAW_MEDRIOD_BLOCK = registerBlock("raw_medriod_block",
-            () -> new Block(BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
+            () -> new Block(BlockBehaviour.Properties.of().strength(5f)
+                    .requiresCorrectToolForDrops().sound(SoundType.STONE)));
 
     public static final RegistryObject<Block> MEDRIOD_ORE = registerBlock("medriod_ore",
-            () -> new Block(BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
+            () -> new DropExperienceBlock(UniformInt.of(2,4), BlockBehaviour.Properties.of().strength(5f)
+                    .requiresCorrectToolForDrops().sound(SoundType.STONE)));
 
     public static final RegistryObject<Block> DEEPSLATE_MEDRIOD_ORE = registerBlock("deepslate_medriod_ore",
-            () -> new Block(BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops().sound(SoundType.DEEPSLATE)));
+            () -> new DropExperienceBlock(UniformInt.of(2,4), BlockBehaviour.Properties.of().strength(5f)
+                    .requiresCorrectToolForDrops().sound(SoundType.DEEPSLATE)));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
